afae651ba6b46d3f843252001b620a5462aa1959
Kuldeep Dixit <dixitk488@gmail.com>
2024-12-09 15:24:45 +0530

    form edit

--- reactwork/reactviteapp/src/App.jsx
+++ reactwork/reactviteapp/src/App.jsx
@@ -1,90 +1,98 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Student from './Student';
-import UseStudentState from "./UseStudentState"
+// import UseStudentState from './UseStudentState';
 import Imagemanipulation from './Imagemanipulation';
 import MyUseEffect from './MyUseEffect';
 import UseFetchAPI from './UseFetchAPI';
-
-function App() {
-  const h1=<h1>Hello World</h1>;
- const mystyle={
-  color:'red',
-  backgroundColor:'yellow'
- }
-//  const studentdata =[{
-//   college:"ABES Engineering College",
-//   name:"Kuldeep",
-//   pic:<img src='https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/srixner.png' height={100} width={100}/>,
-//   branch:"CSE",
-//   roll:"230023947937",
-//   section :"B"
-//  },
-//  {
-//   college:"ABES Engineering College",
-//   name:"Deepak",
-//   pic:<img src='https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/srixner.png' height={100} width={100}/>,
-//   branch:"CSE",
-//   roll:"230023947937",
-//   section :"B"
-//  },
-//  {college:"ABES Engineering College",
-//  name:"Amit",
-//  pic:<img src='https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/srixner.png' height={100} width={100}/>,
-//  branch:"CSE",
-//  roll:"30023947937",
-//  section :"B"}]
-  return (
-//     <div style={{backgroundColor:'cyan'}}>
-//       {h1}
+import Login from './Login';
+import Registration from './Registration';
+function App()
+{
+  const[rdata,setrdata]=useState();
+  const h1=<h1>hello world</h1>;
+  const mystyle={
+    color:'red',
+    backgroundColor:'white'
+  }
+  const studentdata=[{
+    college:"ABES engineering college",
+    name:"Harshit luanda",
+    branch:"cs m",
+    roll:"200",
+    section:"A"
     
-//     <div style={mystyle}>
-//       ABES Engineering College
-//     </div>
-//     <div  style={{display:'flex'}}>
-//       {/* <Student data ={studentdata[1]}/> */
-//       studentdata.map(
-//         (ele)=>{
-//           return <Student data={ele}/>
-//         }
-//       )
-//       }
- 
-//       {/* <Student college="ABES Engineering College" 
-//       name="Rahul"
-//       branch="CSE"
-//       roll="2328392"
-//       section="A"
-      
-//       />
+  },{
+    college:"gl engineering college",
+    name:"Harshit jindabad",
+    branch:"cs c",
+    roll:"2",
+    section:"B"
+  },{
+    college:"Akg engineering college",
+    name:"Harshit bro",
+    branch:"cs b",
+    roll:"234777",
+    section:"b"
+  }
 
-// <Student college="ABES Engineering College" 
-//       name="Ankit"
-//       pic={<img src='https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/srixner.png' height={100} width={100}/>}
-//       branch="CSE"
-//       roll="787878"
-//       section="A"
-      
-//       />
+  
+]
+  return (
+    
+    
+    <div >
+      <div>{JSON.stringify(rdata)}</div>
+      <h2>REGISTRATION FORM</h2>
+    {/* <div><UseStudentState /></div> */}
+    {/* <Imagemanipulation></Imagemanipulation> */}
+    {/* <MyUseEffect /> */}
+    {/* <UseFetchAPI /> */}
+    {/* <Login />} */}
+    
 
-//       <Student college="ABES Engineering College" 
-//       name="Amit Tomer"
-//       branch="CSE"
-//       roll="6754890"
-//       section="A"
-      
-//       /> */}
-//     </div>
-//     </div>
-  <div>
-    <h2>Hello using stateHook</h2>
+    {<Registration regData={setrdata}/>}
     <div>
-      {/* <UseStudentState/> */}
-      {/* <Imagemanipulation/> */}
-      {/* <MyUseEffect/> */}
-      <UseFetchAPI/>
+      <h2>LOGIN FORM</h2>
+      <Login regdata={rdata}/>
     </div>
-  </div>
+    </div>
+    
+
+    // <div style={{backgroundColor:'blueviolet'}}>
+    //   {h1}
+    //   <div style={mystyle}> 
+    //     ABES Engineering college
+    //   </div>
+    //   <div style={{display:"flex"}}>
+    //     {
+    //       studentdata.map(
+    //         (ele)=>{
+    //          return <Student data ={ele}/>
+    //         }
+    //       )
+    //     }
+    //     {/* <Student college="ABES ENGINEERING COLLEGE"
+    //     name="harshit"
+    //     branch="cse"
+    //     Roll="12"
+    //     section="a" 
+    //     pic={<img src='https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/srixner.png' height={100} width={100}/>}
+    //    />
+
+    //     <Student college="ABES ENGINEERING COLLEGE"
+    //     name="sharma"
+    //     branch="cse"
+    //     Roll="13"
+    //     section="b" />
+    //      <Student college="ABES ENGINEERING COLLEGE"
+    //     name="sharma"
+    //     branch="cse"
+    //     Roll="13"
+    //     section="a" />*/}
+
+    //   </div> 
+
+    // </div>
   )
 }
-
 export default App
