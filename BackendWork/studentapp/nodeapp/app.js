6fcd1099efd9f5f74331c80b78e5b8266789849f
Kuldeep Dixit <dixitk488@gmail.com>
2025-03-27 09:12:27 +0530

    adding

--- BackendWork/studentapp/nodeapp/app.js
+++ BackendWork/studentapp/nodeapp/app.js
@@ -1,3 +1,4 @@
+const { stat } = require('fs');
 const http=require('http');
 const fs=require('fs').promises;
 
@@ -6,14 +7,32 @@ const server = http.createServer((req,res)=>{
 
     if(req.url=="/register" && req.method=="POST"){
         let body = '';
+        let arr=[];
 
         req.on('data',chunk=>{
-            body=body+" "+chunk
+            body+=chunk
         })
-        req.on('end',()=>{
-            console.log(body);
+        req.on('end',async()=>{
+            const {name,email,password}=JSON.parse(body);
+            console.log("Name= "+name);
+
+            const data1 = await fs.readFile('student.json',{encoding:'utf-8'});
+            arr=JSON.parse(data1);
+
+            const status = arr.find(ele=>ele.email==email);
+            console.log(status)
+            if(status){
+                return res.end(JSON.stringify({msg:"User's Email is Already Registered"}));
+            }
+            else{
+                arr.push({name,email,password});
+                fs.writeFile('student.json',JSON.stringify(arr));
+                res.end(JSON.stringify({msg:"User is Successfully Registered"}));
+            }
+            
+
         })
-        res.end(JSON.stringify({msg:"User is Successfully Registered"}));
+        
     }
 })
 
